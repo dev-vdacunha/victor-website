b4044a89f8e8ac08677439d0d9d4029a49f62ff8
dev-vdacunha <vdacunha@outlook.com>
2023-10-11 13:31:53 -0300

    Initial commit for backendjavaspringboot

---
+++ backend-java-springboot/src/main/java/com/vdacunha/backendjavaspringboot/BackendJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.vdacunha.backendjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaSpringbootApplication.class, args);
+	}
+
+}
